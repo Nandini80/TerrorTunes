5be70a32f760ad3789d12c4466c28f0e191045e2
Nandini Jindal <nandinijindal010@gmail.com>
2024-01-17 19:55:04 +0530

    axios request

--- client/src/components/Dashboard/dashboard.jsx
+++ client/src/components/Dashboard/dashboard.jsx
@@ -1,66 +1,49 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Carousel from 'react-bootstrap/Carousel';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import carddata from "./data";
 import "./dashboard.css";
+import bg from '../../assets/haunted1.jpg';
 
 function Dashboard() {
   return (
     <>
-    <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-           <Navbar.Brand href="#home">TERROR TUNES</Navbar.Brand>
-           <Navbar.Toggle />
-           <Nav className="me-auto">
-           <Nav.Link href="#home">Collaboration</Nav.Link>
+          <Navbar.Brand href="#home">TERROR TUNES</Navbar.Brand>
+          <Navbar.Toggle />
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Collaboration</Nav.Link>
             <Nav.Link href="#features">games</Nav.Link>
             <Nav.Link href="#pricing">Insturments</Nav.Link>
           </Nav>
-          <Button variant="primary">Enter Details to collaborate</Button>
-          <Button variant="primary">Sign Out</Button>{' '}
+          <Button variant="primary" style={{marginRight:"1rem"}} >Enter Details to collaborate</Button>
+          <Button variant="primary">Sign Out</Button>
         </Container>
       </Navbar>
+
       <div className="howitworks">
-      <Carousel slide={false}>
-      <Carousel.Item>
-        <img src="https://imgs.search.brave.com/0EVuu2d2572nKawM6OCbKKik6sNTLtjCnODwKx2YxjM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9k/YXJrLWdydW5nZS1z/dHlsZS1zY3JhdGNo/ZWQtbWV0YWwtc3Vy/ZmFjZV8xMDQ4LTEy/OTUxLmpwZz9zaXpl/PTYyNiZleHQ9anBn"/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src='https://imgs.search.brave.com/0EVuu2d2572nKawM6OCbKKik6sNTLtjCnODwKx2YxjM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9k/YXJrLWdydW5nZS1z/dHlsZS1zY3JhdGNo/ZWQtbWV0YWwtc3Vy/ZmFjZV8xMDQ4LTEy/OTUxLmpwZz9zaXpl/PTYyNiZleHQ9anBn'/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src='https://imgs.search.brave.com/0EVuu2d2572nKawM6OCbKKik6sNTLtjCnODwKx2YxjM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9k/YXJrLWdydW5nZS1z/dHlsZS1zY3JhdGNo/ZWQtbWV0YWwtc3Vy/ZmFjZV8xMDQ4LTEy/OTUxLmpwZz9zaXpl/PTYyNiZleHQ9anBn'/>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    </div>
-      <div className="dashboardcard">
-        {carddata.map((item, index) => (
-          <Card style={{ width: "18rem" }} key={index} className="">
-            <Card.Img variant="top" src={item.img} />
-            <Card.Body>
-              <Card.Title>{item.title}</Card.Title>
-              <Card.Text>{item.text}</Card.Text>
-              <Button variant="primary" >Play Now</Button>
-            </Card.Body>
-          </Card>
-        ))}
+        <div className="bodypart">
+          <div className="middlepart">
+            <img src={bg} alt="" height="100%" width="100%"/>
+          </div>
+
+          <div className="dashboardcard">
+            {carddata.map((item, index) => (
+              <Card style={{ width: "18rem" }} key={index} className="">
+                <Card.Img variant="top" src={item.img} />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.text}</Card.Text>
+                  <Button variant="primary">Play Now</Button>
+                </Card.Body>
+              </Card>
+
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ client/src/services/axios-config.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+let baseURL = "http://localhost:2000";
+
+const publicReq  = axios.create({
+    baseURL,
+});
+
+const privateReq = axios.create({
+    baseURL,
+});
+
+privateReq.interceptors.request.use((config)=>{
+    const token  = localStorage.getItem("token");
+    if(token)
+    {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export {privateReq};
+export default publicReq;
